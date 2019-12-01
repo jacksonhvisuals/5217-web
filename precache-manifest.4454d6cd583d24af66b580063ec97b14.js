a7db25ed59b10c2b86b71c92d9aafb730ee69dfd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-01 21:11:24 +0000

    Deploy jacksonhvisuals/5217-web to github.com/jacksonhvisuals/5217-web.git:gh-pages

---
+++ precache-manifest.4454d6cd583d24af66b580063ec97b14.js
@@ -0,0 +1,125 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b18f3412aab90e25704defa169bd9c",
+    "url": "CNAME"
+  },
+  {
+    "url": "css-common-dce96796e32f4d9c9e23c94e13d864fd.css"
+  },
+  {
+    "url": "css-dark-cfac71dcf92206060443a21d7525af64.css"
+  },
+  {
+    "url": "css-light-a400cdade6c1fab9b3c7b68c82c81f13.css"
+  },
+  {
+    "url": "css-material-icons-f18b294ba29eab5930f148ad9eaf71f1.css"
+  },
+  {
+    "url": "css-roboto-6a898b069debd8cc2e843dfb51bb77d8.css"
+  },
+  {
+    "revision": "27f45ef6481426517b0377b67fa7738e",
+    "url": "favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "d2fe3ccd0516425713fee600665907e0",
+    "url": "favicon/android-chrome-256x256.png"
+  },
+  {
+    "revision": "80433e833cab017408b571b75989cc11",
+    "url": "favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "26da0d47f4f55eb63e22bddc66222c2d",
+    "url": "favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "ae871ccae98eccefe956c2e9489bddfc",
+    "url": "favicon/browserconfig.xml"
+  },
+  {
+    "revision": "701d477917fc2b1e740e5f76316ad6c3",
+    "url": "favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "f0ef315270b9448de2bb766fafc07061",
+    "url": "favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "ed2798c368731f8832f46f4f9d7456c9",
+    "url": "favicon/favicon.ico"
+  },
+  {
+    "revision": "5d64644845a71f105b7e293934778b20",
+    "url": "favicon/manifest.json"
+  },
+  {
+    "revision": "552e2d80cc3ee6ff57edc759ff1f2af6",
+    "url": "favicon/mask-512x512.png"
+  },
+  {
+    "revision": "aa495293fa96dc881e5d21b5a6a73523",
+    "url": "favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "a49cd6efc1cf6677932b63cf08fa6577",
+    "url": "favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "fonts/Roboto-Light.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "fonts/Roboto-Medium.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "fonts/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "8ad454841f429aa6a1454c6f00de3e47",
+    "url": "images/ic_5217.svg"
+  },
+  {
+    "revision": "b17450d75c1c3a16e45431897ab6993c",
+    "url": "images/ic_notif_white.png"
+  },
+  {
+    "revision": "ad3a5bc1dbaa6e2d3f786e749dd5b4d8",
+    "url": "images/icon.png"
+  },
+  {
+    "revision": "fb506cb23b06a02f8efe604d5f3ec50e",
+    "url": "index.html"
+  },
+  {
+    "revision": "0a1225574ad241390905",
+    "url": "js/bundle.js"
+  },
+  {
+    "revision": "3373cb4b00ac7a1891f1000cc645bbb7",
+    "url": "sound/end_break.wav"
+  },
+  {
+    "revision": "12d183731ff4985a11f525cf9350f3cd",
+    "url": "sound/end_work.wav"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,58 @@
+importScripts("precache-manifest.4454d6cd583d24af66b580063ec97b14.js", "workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+if (!workbox) {
+  console.log('Service Worker - Failed to load Workbox');
+}
+
+workbox.googleAnalytics.initialize();
+
+function matchURL(conditions, options = {}) {
+  const currentOrigin = location.origin,
+        sameOrigin = typeof options.sameOrigin === 'undefined' ? true : options.sameOrigin,
+        acceptIndex = typeof options.acceptIndex === 'undefined' ? false : options.acceptIndex;
+  return function({url, event}) {
+    if (sameOrigin && url.origin !== currentOrigin) {
+      return false;
+    } else if (acceptIndex && url.origin === currentOrigin &&
+      (url.pathname === '' || url.pathname === '/')) {
+      return true;
+    } else if (sameOrigin && url.origin === currentOrigin) {
+      let matched = false;
+      conditions.forEach(condition => {
+        if (!condition instanceof RegExp) condition = new RegExp(condition);
+        if (condition.test(url.href)) {
+          matched = true;
+          return;
+        }
+      });
+      return matched;
+    } else if (!sameOrigin) {
+      let matched = false;
+      conditions.forEach(condition => {
+        if ((condition instanceof RegExp && condition.test(url.href))
+          || (!(condition instanceof RegExp) && url.href.search(condition.toString()) !== -1)) {
+          matched = true;
+          return;
+        }
+      });
+      return matched;
+    } else return false;
+  }
+}
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+const cacheStrategy = new workbox.strategies.CacheFirst({
+  cacheName: 'unmutable-cache',
+  plugins: [ new workbox.expiration.Plugin({
+    maxAgeSeconds: 60 * 60 * 24 * 30 * 12 // 1 year
+  }) ]
+});
+
+// All local routes are unmutable so cache forever
+workbox.routing.registerRoute(
+  matchURL([/.*/], { acceptIndex: true, sameOrigin: true }),
+  cacheStrategy.handle.bind(cacheStrategy) //this will be fixed in v5, see https://github.com/GoogleChrome/workbox/issues/2180
+);
+
